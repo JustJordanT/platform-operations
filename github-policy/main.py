3cfa1e1f0f1ac8bce43c9be7852b89eb98f82813
JustJordanT <jordantay9014@outlook.com>
2021-10-20 17:11:38 -0500

    updated workflow take 22

--- github-policy/main.py
+++ github-policy/main.py
@@ -5,14 +5,13 @@ import pprint
 # from secrets import TOKEN
 from dotenv import load_dotenv, find_dotenv
 
-
 # VARS
 USER = 'justjordant'
 BASE_URL = "https://api.github.com/"
 REPOS_URL = f"{BASE_URL}users/{USER}/repos"
 
 load_dotenv()
-print(os.environ.get("PYTHON_TOKEN"))
+# print(os.environ.get("PYTHON_TOKEN"))
 TOKEN = os.environ.get("PYTHON_TOKEN")
 
 # Passing PATCH data into API calls
@@ -25,24 +24,25 @@ TOKEN = os.environ.get("PYTHON_TOKEN")
 
 
 headers = {
-    'Accept': 'token' + str(TOKEN),
+    'Accept': 'token' + TOKEN,
     'Authorization': 'token %s' % TOKEN,
 }
 
 repo_info = requests.get(REPOS_URL, headers=headers)
 r_dict = repo_info.json()
 
+
 def check_wiki ():
     for repo in r_dict:
         # try:
 
-            if json(repo.get("has_wiki")):
-                # print(repo.get('name'), 'Has wiki feature enabled' ,repo.get('has_wiki'))
-                print('[', repo.get('name'), ']', 'Has wiki feature enabled')
-                # print('This is true')
-        # except AttributeError:
-            # counters is not a dictionary, ignore and move on
-            # pass
+        if repo.get("has_wiki"):
+            # print(repo.get('name'), 'Has wiki feature enabled' ,repo.get('has_wiki'))
+            print('[', repo.get('name'), ']', 'Has wiki feature enabled')
+            # print('This is true')
+    # except AttributeError:
+    # counters is not a dictionary, ignore and move on
+    # pass
 
 
 # def check_issues ():
