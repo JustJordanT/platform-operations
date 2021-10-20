0fa796b3367a33e86397dfefe097db2aeb711aaa
JustJordanT <jordantay9014@outlook.com>
2021-10-20 14:32:17 -0500

    updated workflow take 10 - update func

--- github-policy/main.py
+++ github-policy/main.py
@@ -35,24 +35,28 @@ r_dict = repo_info.json()
 
 def check_wiki ():
     for repo in r_dict:
-        if repo.get('has_wiki'):
-            # print(repo.get('name'), 'Has wiki feature enabled' ,repo.get('has_wiki'))
-            print('[', repo.get('name'), ']', 'Has wiki feature enabled')
-            # print('This is true')
+        try:
+            if repo.get('has_wiki'):
+                # print(repo.get('name'), 'Has wiki feature enabled' ,repo.get('has_wiki'))
+                print('[', repo.get('name'), ']', 'Has wiki feature enabled')
+                # print('This is true')
+        except AttributeError:
+            # counters is not a dictionary, ignore and move on
+            pass
 
 
-def check_issues ():
-    for repo in r_dict:
-        if repo.get('has_issues'):
-            print('[', repo.get('name'), ']', 'Has issues feature enabled')
+# def check_issues ():
+#     for repo in r_dict:
+#         if repo.get('has_issues'):
+#             print('[', repo.get('name'), ']', 'Has issues feature enabled')
+#
+#
+# def check_project ():
+#     for repo in r_dict:
+#         if repo.get('has_projects'):
+#             print('[', repo.get('name'), ']', 'Has issues feature enabled')
 
 
-def check_project ():
-    for repo in r_dict:
-        if repo.get('has_projects'):
-            print('[', repo.get('name'), ']', 'Has issues feature enabled')
-
-
-check_issues()
+# check_issues()
 
 check_wiki()
